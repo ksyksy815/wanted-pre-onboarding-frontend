bae3ceaed34561344cf291a84c799e3bf8487e15
Seoyeon Kim <seoyeonkim815@gmail.com>
2023-08-07 16:37:46 +0900

    Updated routes so that a user gets redirected to /signin

--- src/routes/RouteComponent.tsx
+++ src/routes/RouteComponent.tsx
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { SigninPage, SignupPage, TodoPage, NotFound } from "../pages";
 
 const RouteComponent = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/todo" element={<TodoPage />} />
